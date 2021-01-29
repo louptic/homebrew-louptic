7b91246f73e3939a56cd8c4d8b3b23db4c7f5e85
lou <lou@useoptic.com>
2021-01-29 10:04:03 -0500

    init

---
+++ Api-template.rb
@@ -0,0 +1,36 @@
+require "language/node"
+
+class Api < Formula
+  desc "The Optic CLI"
+  homepage "https://github.com/opticdev/optic"
+  url "{URL}"
+  sha256 "{SHASUM}"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+
+    # Set shebang to Homebrew cellar
+    cellar_location = shell_output("brew --prefix node").strip
+    puts path
+    lines = IO.readlines(path)
+    lines[0] = "#! #{cellar_location}/bin/node"
+    puts "#{HOMEBREW_PREFIX}"
+    puts "#! #{cellar_location}/bin/node"
+    File.open(path, 'w') do |file|
+      file.puts lines
+    end
+
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "@useoptic/cli", shell_output("#{bin}/api --version | awk '{print $1}' ")
+  end
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew-Optic
+
+This repository contains the Formulae for Optic software.
+
+## Software
+
+- **Api** - The Optic Local CLI
+
+## References
+
+- [Optic's Home](https://www.useoptic.com)
+- [Optic on GitHub](https://www.github.com/opticdev/optic)
