7b231fc98f0f5e15cdfd56980687004626392a61
GitHub Package Updater <action@github.com>
2021-01-29 16:28:08 +0000

    Updated Formula

--- Formula/Api.rb
+++ Formula/Api.rb
@@ -4,7 +4,7 @@ class Api < Formula
   desc "The Optic CLI"
   homepage "https://github.com/opticdev/optic"
   url "https://registry.npmjs.org/@useoptic/cli/-/cli-9.0.7.tgz"
-  sha256 "783cd8baadb65e81df821e47723c4f2446fb12f4fe6141341c3acb606c6d28ec"
+  sha256 ""
   license "MIT"
 
   livecheck do
@@ -17,7 +17,7 @@ class Api < Formula
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
 
     # Set shebang to Homebrew cellar
-    cellar_location = shell_output("brew --prefix node").strip
+    cellar_location = %x[brew --prefix node]
     puts path
     lines = IO.readlines(path)
     lines[0] = "#! #{cellar_location}/bin/node"
